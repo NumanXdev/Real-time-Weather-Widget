12e2fdd98abf300cb94a08f297fe8e6668bc3d40
Numan <9256fa@gmail.com>
2024-12-29 22:24:43 +0530

    Using Weather API

--- src/searchBox.jsx
+++ src/searchBox.jsx
@@ -1,11 +1,78 @@
 import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import SendIcon from "@mui/icons-material/Send";
+import "./index.css";
+import { useState } from "react";
+
 
 function SearchBox() {
+  let [City, setCity] = useState("");
+  let API_URL = "https://api.openweathermap.org/data/2.5/weather";
+  let API_KEY = "5fea7cde99cff0329acf34002a07386e";
+
+
+  let getWeatherInfo = async () => {
+
+    let Response = await fetch(
+      `${API_URL}?q=${City}&units=metric&appid=${API_KEY}`
+    );
+
+    if (Response.ok) {
+      let data = await Response.json();
+
+      let result = {
+        temp: data.main.temp,
+        tempMin: data.main.temp_min,
+        tempMax: data.main.temp_max,
+        humidity: data.main.humidity,
+        feelsLike: data.main.feels_like,
+        weather: data.weather[0].description,
+      };
+
+      console.log(result);
+
+    } else {
+      console.error(`Error: ${Response.status} - ${Response.statusText}`);
+    }
+  };
+
+
+  let handleValue = (evt) => {
+    setCity(evt.target.value);
+  };
+
+
+  let handleSubmit = (evt) => {
+    evt.preventDefault();
+    console.log(City);
+    setCity("");
+    getWeatherInfo();
+  };
+
+
   return (
-    <div>
+    <div className="SearchBox">
+
       <h3>Search for the Weather.</h3>
-      <form>
-        <TextField id="standard-basic" label="Standard" variant="standard" />
+      <form onSubmit={handleSubmit}>
+        <TextField
+          id="standard-basic"
+          label="City Name"
+          variant="standard"
+          value={City}
+          onChange={handleValue}
+          required
+        />
+
+        
+        <Button
+          variant="contained"
+          size="medium"
+          endIcon={<SendIcon />}
+          type="Submit"
+        >
+          Send
+        </Button>
       </form>
     </div>
   );
